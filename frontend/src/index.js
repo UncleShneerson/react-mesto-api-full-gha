f7a3b056c8462838f5c3a543f1c1e2485a170ecd
Eugene Krasnoshlykov <UncleShneerson@yandex.ru>
2023-09-03 20:57:12 -0300

    edit router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./components/App";
@@ -11,10 +11,9 @@ const root = ReactDOM.createRoot(rootDiv);
 
 root.render(
   <React.StrictMode>
-    {/* заменил brouser router на hash - чтобы отражалось на Gitpages */}
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
